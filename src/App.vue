078b24ed025d39e17b821eefa9be898e5182dacb
gredezcici <gredezcici@163.com>
2017-05-31 00:57:08 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+
+
+    <el-row >
+      <el-col :span="4">
+        <sidebar></sidebar>
+      </el-col>
+
+
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+
+  import Sidebar from './components/Nav'
+  import Top from './components/Topbar'
+  import card from './components/Card'
+  export default {
+    name: 'app',
+    components: {
+       Sidebar
+    }
+  }
+</script>
+<style >
+.card{
+  margin-top: 10px;
+}
+  el-row{
+    height:100%;
+  }
+</style>
+
---
+++ src/components/Card.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <span style="line-height: 36px;">Card name</span>
+      <el-button style="float: right;" type="primary">Operation button</el-button>
+    </div>
+    <div v-for="o in 4" :key="o" class="text item">
+      {{'List item ' + o }}
+    </div>
+  </el-card>
+</template>
+
+<script>
+  export default {
+    name: 'card'
+
+
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    padding: 18px 0;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 480px;
+  }
+</style>
